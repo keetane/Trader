f63e6bf0b0d3666e370d5b7b14293e39a0de269e
keetane <yu.takahashi.k@gmail.com>
2024-05-07 22:50:53 +0900

    1min_candle.py and gitignore was created

---
+++ 1min_candle.py
@@ -0,0 +1,121 @@
+#%%
+import pandas as pd
+import yfinance as yf
+from datetime import datetime, timedelta, time
+
+# 銘柄リストとデータの取得、保存
+ticker_dict = {
+    '' : '',
+    '東京エレクトロン' : '8035',
+    '霞ヶ関' : '3498',
+    'アドバンテスト' : '6857',
+    'ソシオネクスト' : '6526',
+    'リクルート' : '6098',
+    'QPS' : '5595',
+    'ルネサス' : '6723',
+    'レーザーテック' : '6920',
+    'ディスコ' : '6146',
+    '日経レバダブル' : '1579',
+    'さくらインターネット' : '3778',
+    'SBG': '9984',
+    '名村造船所' : '7014',
+    '川崎汽船' : '9107',
+    '日本郵船' : '9101',
+    '三井E&S' : '7003',
+    '売れるネット広告社' : '9235',
+    'ispace' : '9348',
+    '住石HD' : '1514',
+}
+
+
+# 日付の取得
+past = datetime.now() - timedelta(10)
+past_str = past.strftime('%Y-%m-%d')
+
+for ticker in list(ticker_dict.values()):
+
+    # データの取得
+    data = yf.download(
+        tickers = ticker + '.T',
+        start = past_str,
+        )
+    data['delta'] = data['Close'].diff()
+    data['%'] = data['Close'].pct_change() * 100
+    data['Close_-1'] = data['Close'].shift()
+
+    df = yf.download(tickers=ticker + '.T',
+                    interval='1m',
+                    )
+
+    # 時間を追加
+    df['time'] = [t.time() for t in df.index]
+    df['day'] = [t.date() for t in df.index]
+
+    # close間の差を計算
+    df['delta'] = df['Close'].diff()
+    df['%'] = df['Close'].pct_change() * 100
+
+    # 前日との差を計算
+    df['Close_-1'] = df['day'].map(lambda x : data['Close_-1'][str(x)])
+    df['delta_yd'] = df['Close'] - df['Close_-1']
+    df['pct_yd'] = df['delta_yd']/df['Close_-1']*100
+
+    # SMAを計算 
+    df["SMA_short"] = df["Close"].rolling(window=10).mean() 
+    df["SMA_middle"] = df["Close"].rolling(window=25).mean()
+    df["SMA_long"] = df["Close"].rolling(window=75).mean()
+
+    def macd(df):
+        FastEMA_period = 12  # 短期EMAの期間
+        SlowEMA_period = 26  # 長期EMAの期間
+        SignalSMA_period = 9  # SMAを取る期間
+        df["MACD"] = df["Close"].ewm(span=FastEMA_period).mean() - df["Close"].ewm(span=SlowEMA_period).mean()
+        df["Signal"] = df["MACD"].rolling(SignalSMA_period).mean()
+        return df
+    
+
+    def rsi(df):
+        # 前日との差分を計算
+        df_diff = df["Close"].diff(1)
+    
+        # 計算用のDataFrameを定義
+        df_up, df_down = df_diff.copy(), df_diff.copy()
+        
+        # df_upはマイナス値を0に変換
+        # df_downはプラス値を0に変換して正負反転
+        df_up[df_up < 0] = 0
+        df_down[df_down > 0] = 0
+        df_down = df_down * -1
+        
+        # 期間14でそれぞれの平均を算出
+        df_up_sma14 = df_up.rolling(window=14, center=False).mean()
+        df_down_sma14 = df_down.rolling(window=14, center=False).mean()
+    
+        # RSIを算出
+        df["RSI"] = 100.0 * (df_up_sma14 / (df_up_sma14 + df_down_sma14))
+    
+        return df
+    
+    # MACDを計算する
+    df = macd(df)
+    
+    # RSIを算出
+    df = rsi(df)
+
+    # tickerをdfに追加
+    df['ticker'] = ticker
+
+    # 保存データとの結合
+    try:
+        df = pd.concat(
+            [pd.read_csv(
+                '~/Documents/trader/1min/' + ticker + '.csv',
+                parse_dates=['Datetime'], 
+                index_col='Datetime'
+                ), 
+            df])
+    except:
+        pass
+
+    # データを保存
+    df.drop_duplicates().to_csv('~/Documents/trader/1min/' + ticker + '.csv')
